909a01552e4da6dcbf3595f63d694b1db7b1eb13
MexL <madiajijah7@gmail.com>
2019-05-18 00:44:48 +0800

    update profile html css js

--- bootstrap/js/profile.js
+++ bootstrap/js/profile.js
@@ -1,128 +1,119 @@
-var canvas = $('canvas')[0];
-var context = canvas.getContext('2d');
+(function () {
+    'use strict';
 
-var Dots = [];
-var ID = 0;
-var colors = ['#FF9900', '#424242', '#BCBCBC', '#3299BB', '#B9D3B0', '#81BDA4', '#F88F79', '#F6AA93'];
-var maximum = 100;
+    const attributes = {
+        username: 'data-username',
+        maxRepos: 'data-max-repos',
+        sortBy: 'data-sort-by',
+        headerText: 'data-header-text'
+    };
 
-function Dot() {
-  this.active = true;
-  this.id = ID; ID++;
-  
-  this.diameter = 2 + Math.random() * 7;
+    window.widgetGenerator = {
+        regenerate: regenerate
+    };
 
-  this.x = Math.round(Math.random() * canvas.width);
-  this.y = Math.round(Math.random() * canvas.height);
-  
-  this.velocity = {
-    x: (Math.random() < 0.5 ? -1 : 1) * Math.random() * 0.4,
-    y: (Math.random() < 0.5 ? -1 : 1) * Math.random() * 0.4
-  };
+    function regenerate(options) {
+        const attributesTemplate = Object.keys(options)
+            .map(option => {
+                const attribute = attributes[option];
+                const value = options[option];
+                if (!attribute) {
+                    return '';
+                }
+                return `\n\t${attribute}="${value}"`;
+            })
+            .join('');
 
-  this.alpha = 0.1;
-  this.maxAlpha = this.diameter < 5 ? 0.3 : 0.8;
-  this.hex = colors[Math.round(Math.random() * 7)];
-  this.color = HexToRGBA(this.hex, this.alpha);
-}
+        return `<div id="github-card" ${attributesTemplate}>\n</div>`;
+    }
+})();
+(function (GitHubCard, widgetGenerator) {
+    "use strict";
 
-Dot.prototype = {
-  Update: function() {
-    if(this.alpha <= this.maxAlpha) {
-      this.alpha += 0.005;
-      this.color = HexToRGBA(this.hex, this.alpha);
+  // Generating new widget from user input
+    document.addEventListener('DOMContentLoaded', function() {
+        var options = {
+            template: '#github-card-demo',
+            sortBy: 'stars', // possible: 'stars', 'updateTime'
+            headerText: 'Most starred repositories',
+            maxRepos: 5
+        };
+
+        var widget = new GitHubCard(options);
+        widget.init();
+        regenerate(options);
+
+        initSortingControl(options, refreshWidget);
+        initRepositoriesControl(options, refreshWidget);
+        initUserControl(options, initWidget);
+
+        function initWidget(options) {
+            widget = new GitHubCard(options);
+            widget.init();
+            regenerate(options);
+        }
+
+        function refreshWidget(updatedOptions) {
+            widget.refresh(updatedOptions);
+            regenerate(updatedOptions);
+        }
+
+        function regenerate(updatedOptions) {
+            var textarea = document.getElementById("install-code");
+            textarea.value = widgetGenerator.regenerate(updatedOptions);
+        }
+    });
+
+    // Sort repository acording to
+    // radio inputs on website
+    function initSortingControl(options, refreshWidget) {
+        var $sortingRadios = document.querySelectorAll('.choose-repo-sorting label');
+
+        // sort by update time
+        $sortingRadios[0].addEventListener('click', function (element) {
+            element.target.classList.add('active');
+            $sortingRadios[1].classList.remove('active');
+
+            options.sortBy = 'updateTime';
+            options.headerText = element.target.textContent + ' repositories';
+
+            refreshWidget(options);
+        });
+
+        // sort by starrgazers
+        $sortingRadios[1].addEventListener('click', function (element) {
+            element.target.classList.add('active');
+            $sortingRadios[0].classList.remove('active');
+
+            options.sortBy = 'stars';
+            options.headerText = element.target.textContent + ' repositories';
+
+            refreshWidget(options);
+        });
     }
 
-    this.x += this.velocity.x;
-    this.y += this.velocity.y;
+    // Manipulating the number of repositories
+    function initRepositoriesControl(options, refreshWidget) {
+        var $inputNumber = document.getElementById('gh-reposNum');
 
-    if(this.x > canvas.width + 5 || this.x < 0 - 5 || this.y > canvas.height + 5 || this.y < 0 - 5) {
-      this.active = false;
-    }
-  },
+        $inputNumber.onchange = function() {
+            options.maxRepos = $inputNumber.value;
 
-  Draw: function() {
-    context.strokeStyle = this.color;
-    context.fillStyle = this.color;
-    context.save();
-    context.beginPath();
-    context.translate(this.x, this.y);
-    context.moveTo(0, -this.diameter);
-
-    for (var i = 0; i < 7; i++)
-    {
-      context.rotate(Math.PI / 7);
-      context.lineTo(0, -(this.diameter / 2));
-      context.rotate(Math.PI / 7);
-      context.lineTo(0, -this.diameter);
+            refreshWidget(options);
+        };
     }
 
-    if(this.id % 2 == 0) {
-      context.stroke();
-    } else {
-      context.fill();
+    // Creating brand new widget instance
+    // for user that we type in input
+    function initUserControl(options, fn) {
+        var $input = document.getElementById('gh-uname'),
+            $submit = document.getElementById('gh-uname-submit');
+
+        $submit.addEventListener('click', function (element) {
+            options.username = $input.value;
+            fn(options);
+
+            element.preventDefault();
+        });
     }
-    
-    context.closePath();
-    context.restore();
-  }
-}
-
-function Update() {
-  GenerateDots();
-
-  Dots.forEach(function(Dot) {
-    Dot.Update();
-  });
-
-  Dots = Dots.filter(function(Dot) {
-    return Dot.active;
-  });
-
-  Render();
-  requestAnimationFrame(Update);
-}
-
-function Render() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  Dots.forEach(function(Dot) {
-    Dot.Draw();
-  });
-}
-
-function GenerateDots() {
-  if(Dots.length < maximum) {
-    for(var i = Dots.length; i < maximum; i++) {
-      Dots.push(new Dot());
-    }
-  }
-
-  return false;
-}
-
-function HexToRGBA(hex, alpha) {
-  var red = parseInt((TrimHex(hex)).substring(0, 2), 16);
-  var green = parseInt((TrimHex(hex)).substring(2, 4), 16);
-  var blue = parseInt((TrimHex(hex)).substring(4, 6), 16);
-
-  return 'rgba(' + red + ', ' + green + ', ' + blue + ', ' + alpha + ')';
-}
-
-function TrimHex(hex) {
-  return (hex.charAt(0) == "#") ? hex.substring(1, 7) : hex;
-}
-
-function WindowSize(width, height) {
-  if(width != null) { canvas.width = width; } else { canvas.width = window.innerWidth; }
-  if(height != null) { canvas.height = height; } else { canvas.height = window.innerHeight; }
-  
-}
-
-$(window).resize(function() {
-  Dots = [];
-  WindowSize();
-});
-
-WindowSize();
-GenerateDots();
-Update();
+})(window.GitHubCard, window.widgetGenerator);
