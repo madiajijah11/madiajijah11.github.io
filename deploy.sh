1ea2956963b66df37e0f16583c20f5dc206461a7
Rappwalk <madiajijah7@gmail.com>
2022-06-13 23:46:02 +0800

    change to default deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:madiajijah11/madiajijah11.github.io.git main
+# git push -f git@github.com:madiajijah11/madiajijah11.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
