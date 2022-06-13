278838061783b34938e6c9fe5f812e6f65525224
Rappwalk <madiajijah7@gmail.com>
2022-06-14 02:55:01 +0800

    remove min heigh and min width

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,9 @@ function App() {
       <hr />
 
       {/* My Name */}
-      <div>
+      <div className='Text-container'>
         <h1>Hello, I'm Dian aka Rappwalk aka MexL aka madiajijah11 aka GenzoDR</h1>
+        <h1><i className="bi bi-quote"></i></h1>
         <p>I'm a full stack developer, web developer & mobile developer.</p>
       </div>
       <br />
